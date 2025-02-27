0df2dfe996f28f3f12ae45125050a24cc827152e
OmkarSawantt <omkarsawantttt4@gmail.com>
2025-02-27 15:21:46 +0530

    first commit

---
+++ src/app/assets/porfolioData.js
@@ -0,0 +1,122 @@
+export const ProjectsData = [
+  {
+    "id": "weather-app-react-native",
+    "Title": "Weather App in React Native",
+    "Description": "A mobile application that provides real-time weather updates using API integration.",
+    "Features": [
+      "Current weather data",
+      "Search for different locations",
+      "User-friendly UI",
+      "Supports both Android and iOS"
+    ],
+    "Img":"https://firebasestorage.googleapis.com/v0/b/uploadingfile-1f51f.appspot.com/o/portfolio%2Fprojects%2Ffile%20cover%20-%201.png?alt=media&token=f265d480-26b1-4783-866f-76bd8a4230ba",
+    "Github": "https://github.com/OmkarSawantt/ReactNativeWeatherApp",
+    "Link": "",
+    "TechStack": ["React Native", "API Integration"]
+  },
+  {
+    "id": "chat-app",
+    "Title": "Chat App",
+    "Description": "A real-time chat application that enables users to send and receive messages instantly.",
+    "Features": [
+      "Real-time messaging",
+      "User authentication",
+      "Group and private chats",
+      "Message notifications"
+    ],
+    "Img":"https://firebasestorage.googleapis.com/v0/b/uploadingfile-1f51f.appspot.com/o/portfolio%2Fprojects%2FScreenshot%202025-02-26%20143632.png?alt=media&token=96f3ae03-8afd-49b8-b4c0-d44bb447125c",
+    "Github": "https://github.com/OmkarSawantt/ChatApp",
+    "Link": "https://sonorous.vercel.app",
+    "TechStack": ["React.js", "Node.js", "Socket.io", "MongoDB"]
+  },
+  {
+    "id": "report-generating-web-app",
+    "Title": "Report Generating Web App",
+    "Description": "A web application that allows users to generate detailed reports by inputting event data, including images and other details.",
+    "Features": [
+      "User input for event details",
+      "Multiple report templates",
+      "PDF generation",
+      "Image upload functionality"
+    ],
+    "Img":"https://firebasestorage.googleapis.com/v0/b/uploadingfile-1f51f.appspot.com/o/portfolio%2Fprojects%2FScreenshot%202025-02-26%20142106.png?alt=media&token=1f0d11a0-e66d-4da1-95fd-c1e2438c2f12",
+    "Github": "https://github.com/OmkarSawantt/ReportGenerator",
+    "Link": "",
+    "TechStack": ["React.js", "Node.js", "Express.js", "MongoDB"]
+  },
+  {
+    "id": "employee-management-system-backend",
+    "Title": "Employee Management System Backend",
+    "Description": "A backend system for managing employees, including features for adding, updating, and deleting employee records, authentication, and role-based access control.",
+    "Features": [
+      "CRUD operations for employee records",
+      "Authentication and authorization",
+      "Role-based access control",
+      "Database integration with MongoDB"
+    ],
+    "Img":"https://firebasestorage.googleapis.com/v0/b/uploadingfile-1f51f.appspot.com/o/portfolio%2Fprojects%2FEMS.jpeg?alt=media&token=bc12d12f-2df3-4344-a33f-1f9a5157dad7",
+    "Github": "https://github.com/OmkarSawantt/Employee_management_Backend",
+    "Link": "https://employee-management-backend-kwaj.onrender.com",
+    "TechStack": ["Node.js", "Express.js", "MongoDB"]
+  },
+  {
+    "id": "blogging-application",
+    "Title": "Blogging Application",
+    "Description": "A blogging platform where users can create, edit, and publish blog posts with support for multimedia content.",
+    "Features": [
+      "User authentication",
+      "Rich text editor",
+      "Image and video uploads",
+      "Comment and like system"
+    ],
+    "Img":"https://firebasestorage.googleapis.com/v0/b/uploadingfile-1f51f.appspot.com/o/portfolio%2Fprojects%2Fblog-hosting-sites.jpg?alt=media&token=18587f88-75b0-4551-afd1-ee0ce9070777",
+    "Github": "https://github.com/OmkarSawantt/BlogBytes_Frontend",
+    "Link": "",
+    "TechStack": ["React.js", "Node.js", "Express.js", "MongoDB"]
+  },
+  {
+    "id": "todo-app-mern",
+    "Title": "To-Do App in MERN Stack",
+    "Description": "A task management application that allows users to create, update, and delete tasks with a user-friendly interface.",
+    "Features": [
+      "User authentication",
+      "Create, update, and delete tasks",
+      "Task completion tracking",
+      "Responsive UI"
+    ],
+    "Img":"https://firebasestorage.googleapis.com/v0/b/uploadingfile-1f51f.appspot.com/o/portfolio%2Fprojects%2FScreenshot%202025-02-26%20140854.png?alt=media&token=6260a600-f805-4a4e-b316-2a7fb848cb36",
+    "Github": "https://github.com/OmkarSawantt/todo",
+    "Link": "",
+    "TechStack": ["MongoDB", "Express.js", "React.js", "Node.js"]
+  },
+
+];
+
+// const displayedCertificates = showAllCertificates ? certificates : certificates.slice(0, initialItems);
+// console.log(displayedCertificates);
+export const CertificatesData =[
+  {
+    'Img':'https://firebasestorage.googleapis.com/v0/b/uploadingfile-1f51f.appspot.com/o/portfolio%2Fcertificates%2FFullStack_Ibm.jpg?alt=media&token=505f4c2f-c7f0-46b9-85de-d8009c03e878'
+  },
+  {
+    'Img':'https://firebasestorage.googleapis.com/v0/b/uploadingfile-1f51f.appspot.com/o/portfolio%2Fcertificates%2FFrontend_Ibm.jpg?alt=media&token=9ede0267-e7d3-402d-bc26-7303336152e8'
+  },
+  {
+    'Img':'https://firebasestorage.googleapis.com/v0/b/uploadingfile-1f51f.appspot.com/o/portfolio%2Fcertificates%2FMeta_Frontend.jpg?alt=media&token=6dd8e7f7-daa5-4072-bd30-b2a22ec1eae3'
+  },
+  {
+    'Img':'https://firebasestorage.googleapis.com/v0/b/uploadingfile-1f51f.appspot.com/o/portfolio%2Fcertificates%2FIBM-Backend.jpg?alt=media&token=cf554473-ae57-45ec-8964-3be46e7f8904'
+  },
+  {
+    'Img':'https://firebasestorage.googleapis.com/v0/b/uploadingfile-1f51f.appspot.com/o/portfolio%2Fcertificates%2FIbm_js.jpg?alt=media&token=699ecad0-4444-4a99-aed7-4d2bc38db622'
+  },
+  {
+    'Img':'https://firebasestorage.googleapis.com/v0/b/uploadingfile-1f51f.appspot.com/o/portfolio%2Fcertificates%2FNoSql.jpg?alt=media&token=959c3e17-bc78-4bf8-9ad0-2742c70d0cbd'
+  },
+  {
+    'Img':'https://firebasestorage.googleapis.com/v0/b/uploadingfile-1f51f.appspot.com/o/portfolio%2Fcertificates%2Fjava.jpg?alt=media&token=19467b2c-903f-4ffb-a274-25d47025981e'
+  },
+  {
+    'Img':'https://firebasestorage.googleapis.com/v0/b/uploadingfile-1f51f.appspot.com/o/portfolio%2Fcertificates%2Fpython.jpg?alt=media&token=3b41f1e6-b85c-4179-bac9-a8e7512560c5'
+  }
+]
